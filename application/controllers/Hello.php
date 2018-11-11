49ab45e38f9afd5cbcf4d7797d5164ac934f1241
dangtai0709 <36461370+dangtai0709@users.noreply.github.com>
2018-11-11 11:14:53 +0700

    em update cho no cho kia

--- application/controllers/Hello.php
+++ application/controllers/Hello.php
@@ -6,7 +6,7 @@ class Hello extends CI_Controller
 {
 	 function __construct()
     {
-        // this is your constructor
+        // this is your constructor abc abc
         parent::__construct();
         $this->load->helper('form');
         $this->load->helper('url');
